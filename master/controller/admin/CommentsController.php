a520aa92d3eafe821ec9c9e87d8c86f6b0cdd7fe
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-06 01:39:04 +0200

    Method index

--- master/controller/admin/CommentsController.php
+++ master/controller/admin/CommentsController.php
@@ -5,5 +5,11 @@ use \master\model\MasterFactory;
 
 class CommentsController extends AdminController
 {
-    
+    public function index()
+    {
+        $master = MasterFactory::getInstance();
+        $req = $master->getTable('comments')->commentsByPosts();
+        
+        $this->render('backend/comments/index', compact('req'));
+    }
 }
