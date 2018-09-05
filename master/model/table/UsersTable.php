ba2df97d0de34913f5c8db96447af0f2de7a8563
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-05 22:28:53 +0200

    Method usersExists and addUser

--- master/model/table/UsersTable.php
+++ master/model/table/UsersTable.php
@@ -3,5 +3,35 @@ namespace master\model\table;
 
 class UsersTable extends Table
 {
+    /*
+    * function usersExists
+    * @param string username
+    * @param string mail
+    * @return array
+    */
+    public function usersExists($username, $mail)
+    {
+        $req = $this->database->prepare(
+            'SELECT COUNT(*) AS user
+            FROM '. $this->table .'
+            WHERE username = ? OR mail = ?',
+            [$username, $mail],
+            true
+        );
+        return $req;
+    }
     
+    /*
+    * function addUser
+    * @param string username
+    * @param string password
+    * @param string mail
+    */
+    public function addUser($username, $password, $mail)
+    {
+        $this->database->prepare(
+            'INSERT INTO '. $this->table .' SET username = ?, password = ?, mail = ?',
+            [$username, $password, $mail]
+        );
+    }
 }
