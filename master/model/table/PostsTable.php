34cf72b4bb2522b75d6492d07d77b02b7d83b796
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-05 21:24:55 +0200

    Method allWithLimit

--- master/model/table/PostsTable.php
+++ master/model/table/PostsTable.php
@@ -15,4 +15,25 @@ class PostsTable extends Table
         );
         return $req;
     }
+    
+    /*
+	* function allWithLimit
+    * @param int
+    * @return array
+    */
+    public function allWithLimit($limit)
+    {
+        $req = $this->database->prepareWithLimit(
+            'SELECT posts.id, posts.title, posts.intro, posts.content, posts.author, posts.image,
+            MONTH(date_post) AS month,
+            DAY(date_post) AS day,
+            YEAR(date_post) AS year,
+            DATE_FORMAT(date_post, \'%H:%i:%s\') AS hour
+            FROM '. $this->table .'
+            ORDER BY date_post DESC
+            LIMIT :limit, 4',
+            $limit
+        );
+        return $req;
+    }
 }
