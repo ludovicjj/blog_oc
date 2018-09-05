c7478871d0d707a16bdd5707df7a2566dabce331
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-06 01:07:28 +0200

    Update rooter

--- index.php
+++ index.php
@@ -35,6 +35,22 @@ switch ($p) {
         $controller = new master\controller\UsersController();
         $controller->logout();
         break;
+    case 'admin.posts.index':
+        $controller = new master\controller\admin\PostsController();
+        $controller->index();
+        break;
+    case 'admin.posts.add':
+        $controller = new master\controller\admin\PostsController();
+        $controller->add();
+        break;
+    case 'admin.posts.edit':
+        $controller = new master\controller\admin\PostsController();
+        $controller->edit();
+        break;
+    case 'admin.posts.delete':
+        $controller = new master\controller\admin\PostsController();
+        $controller->delete();
+        break;
     default:
         $controller = new master\controller\HomeController();
         $controller->index();
