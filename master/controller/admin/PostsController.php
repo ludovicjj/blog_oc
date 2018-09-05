114195a8ca0b110289ef6c614360cfc6c7fc9b87
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-06 00:46:14 +0200

    Method add

--- master/controller/admin/PostsController.php
+++ master/controller/admin/PostsController.php
@@ -16,4 +16,49 @@ class PostsController extends AdminController
         }
         $this->render('backend/posts/index', compact('posts'));
     }
+    
+    public function add()
+    {
+        $master = MasterFactory::getInstance();
+        $error = null;
+        $message = null;
+        
+        if ($_POST) {
+            $error = true;
+            $message = 'Tous les champs sont obligatoires';
+            
+            if (!empty($_POST['title'] &&
+                $_POST['intro'] &&
+                $_POST['content'] &&
+                $_POST['author'] &&
+                $_FILES['image']['name'])) {
+                $image = $_FILES['image']['name'];
+                $extensions_valides = array('jpg', 'jpeg', 'png');
+                $extension_upload = strtolower(substr(strrchr($image, '.'), 1));
+                
+                if ($_FILES['image']['error'] > 0) {
+                    $error = "Erreur lors du transfert de l\'image";
+                } else {
+                    if (in_array($extension_upload, $extensions_valides)) {
+                        $path_image ='public/img/' . $_FILES['image']['name'];
+                        move_uploaded_file(
+                            $_FILES['image']['tmp_name'],
+                            $path_image
+                        );
+                        $master->getTable('posts')->addPost(
+                            $_POST['title'],
+                            $_POST['intro'],
+                            $_POST['content'],
+                            $_POST['author'],
+                            $path_image
+                        );
+                        header('Location:index.php?p=admin.posts.index');
+                    } else {
+                        $error = 'Extension incorrecte (uniquement ".jpg", ".jpeg", ".png")';
+                    }
+                }
+            }
+        }
+        $this->render('backend/posts/add', compact('error', 'message'));
+    }
 }
