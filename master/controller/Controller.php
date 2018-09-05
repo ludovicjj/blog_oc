3d7953251146dd94990f9ea5b70f8cf7cc8f6bb8
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-05 18:50:50 +0200

    controller (main)

---
+++ master/controller/Controller.php
@@ -0,0 +1,39 @@
+<?php
+namespace master\controller;
+
+use \master\model\MasterFactory;
+
+class Controller
+{
+    protected $viewRoot;
+    protected $template = 'default';
+    protected $title = 'blog';
+    
+    public function __construct()
+    {
+        $this->viewRoot = 'master/view/';
+    }
+    
+    protected function render($view, $variables = [])
+    {
+        extract($variables);
+        
+        ob_start();
+        require($this->viewRoot . $view .'.php');
+        $content = ob_get_clean();
+        
+        $title = $this->getTitle();
+        require($this->viewRoot . 'template/' . $this->template . '.php');
+    }
+    
+    
+    protected function getTitle()
+    {
+        return $this->title;
+    }
+    
+    protected function setTitle($title)
+    {
+        return $this->title = $title;
+    }
+}
