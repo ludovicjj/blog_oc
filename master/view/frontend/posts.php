3a30e8465d756de970ea52ae2b41271c70217462
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-05 21:26:58 +0200

    Update Articles page

--- master/view/frontend/posts.php
+++ master/view/frontend/posts.php
@@ -1,6 +1,25 @@
 <div class="container-fluid conteneur">
     <div class="container bloc-posts">
         <div class="col-lg-9 col-md-12 col-sm-12">
+            <div class="row">
+                <?php foreach ($post as $posts) : ?>
+                <div class="news">
+                    <div class="col-sm-12 news-image">
+                        <a href="<?= $posts->getUrl();?>"><img src="<?= $posts->getImage();?>"/></a>
+                    </div>
+                    <div class="col-sm-12 news-text">
+                        <h1><a href="<?= $posts->getUrl();?>"><?= $posts->getTitle();?></a></h1>
+                        <small>
+                            Par <?= $posts->getAuthor();?>, 
+                            le <?= $posts->getDay() .' '.$posts->getMonth().' '.$posts->getYear();?>
+                        </small>
+                        <p><?= $posts->getIntro();?></p>
+                    </div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+        <div class="col-lg-9 col-md-12 col-sm-12">
             <ul class="pagination justify-content-center">
                 <?php for ($i = 0; $i < $paging; $i++) : ?>
                 <li class="page-item
