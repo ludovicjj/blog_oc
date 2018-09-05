f0f60ad600e0dfd746ed94d17acf9fac1273b8cf
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-05 23:04:04 +0200

    Method login

--- master/controller/UsersController.php
+++ master/controller/UsersController.php
@@ -40,4 +40,38 @@ class UsersController extends Controller
         }
         $this->render('frontend/register', compact('error', 'message'));
     }
+    
+    public function login()
+    {
+        $this->setTitle('Connexion');
+        $master = MasterFactory::getInstance();
+        $users = $master->getTable('users');
+        
+        $error = null;
+        $message = null;
+        
+        if ($_POST) {
+            $error = true;
+            $message = 'Tous les champs sont obligatoires';
+            
+            if (!empty($_POST['username'] && $_POST['password'])) {
+                $req = $users->loginUser(
+                    $_POST['username'],
+                    sha1($_POST['password'])
+                );
+                if ($req) {
+                    $entity = $users->getEntity($req);
+                    
+                    $_SESSION['username'] = $entity->getUsername();
+                    $_SESSION['statut'] = $entity->getStatut();
+                    
+                    header('Location:index.php?p=index');
+                } else {
+                    $message = 'Identifiants incorrects<br/>';
+                    $message .= 'Inscrivez vous en cliquant <a href="index.php?p=register">ici</a>';
+                }
+            }
+        }
+        $this->render('frontend/login', compact('error', 'message'));
+    }
 }
