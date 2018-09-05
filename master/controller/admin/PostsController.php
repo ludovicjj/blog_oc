43980c5fd14982405c3935fc5554b7b648807383
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-06 00:44:02 +0200

    Method index

--- master/controller/admin/PostsController.php
+++ master/controller/admin/PostsController.php
@@ -5,5 +5,15 @@ use \master\model\MasterFactory;
 
 class PostsController extends AdminController
 {
-    
+    public function index()
+    {
+        $master = MasterFactory::getInstance();
+        $req_posts = $master->getTable('posts')->all();
+        $posts = array();
+        foreach ($req_posts as $data_posts) {
+            $entity_posts = $master->getTable('posts')->getEntity($data_posts);
+            $posts[] = $entity_posts;
+        }
+        $this->render('backend/posts/index', compact('posts'));
+    }
 }
