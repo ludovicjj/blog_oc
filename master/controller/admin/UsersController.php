4d8354c2a302b1d486e6be6ec3d14f495b4c50b9
Votre nom ou pseudo <jahanlud@gmail.com>
2018-09-06 02:02:57 +0200

    Method index

--- master/controller/admin/UsersController.php
+++ master/controller/admin/UsersController.php
@@ -5,5 +5,15 @@ use \master\model\MasterFactory;
 
 class UsersController extends AdminController
 {
-    
+    public function index()
+    {
+        $master = MasterFactory::getInstance();
+        $req_users = $master->getTable('users')->all();
+        $users = array();
+        foreach ($req_users as $data_users) {
+            $entity_users = $master->getTable('users')->getEntity($data_users);
+            $users[] = $entity_users;
+        }
+        $this->render('backend/users/index', compact('users'));
+    }
 }
